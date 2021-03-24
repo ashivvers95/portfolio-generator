d8baa014b75fb7f4f809f0790e95faa451ecc700
Shivvers <ashivvers95@gmail.com>
2021-03-24 15:24:15 -0700

    NPM inquirer added

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
-console.log(profileDataArgs);
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw (err);
 
-console.log(name,github);
+//   console.log('Portfolio complete! Checkout index.html to see the output!')
+// });
 
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw (err);
-
-  console.log('Portfolio complete! Checkout index.html to see the output!')
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
